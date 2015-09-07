ab6f05a1788d7a2104ac5195a50c0ebaeb2eaf28
Jingbo Shang <shang7@dmserv4.cs.illinois.edu>
2015-09-06 21:26:59 -0500

    fix a bug in online parser when there are floating numbers in the text.

--- src/online_query/segphrase_parser.cpp
+++ src/online_query/segphrase_parser.cpp
@@ -43,7 +43,7 @@ void loadRankList(string filename, int topN)
         order.push_back(make_pair(score, word));
     }
     sort(order.rbegin(), order.rend());
-    
+
     if (topN < 0) {
         topN = order.size();
     }
@@ -56,6 +56,67 @@ void loadRankList(string filename, int topN)
     }
 }
 
+string translate(vector<pair<string, bool>> &segments, bool clean_mode, string &origin, string &text, vector<string> &betweens, int &index)
+{
+    string answer = "";
+    if (clean_mode) {
+        for (size_t i = 0; i < segments.size(); ++ i) {
+            if (segments[i].second) {
+                answer += "[";
+            }
+            answer += segments[i].first;
+            if (segments[i].second) {
+                answer += "]";
+            }
+            answer += " ";
+        }
+        answer += "$ ";
+    } else {
+        size_t last = 0;
+        if (segments.size() == 0) {
+            answer += origin;
+        } else {
+            for (size_t i = 0; i < segments.size(); ++ i) {
+                size_t st = last;
+                while (text[st] != segments[i].first[0]) {
+                    ++ st;
+                }
+                size_t ed = st;
+                for (size_t j = 0; j < segments[i].first.size(); ++ j) {
+                    while (text[ed] != segments[i].first[j]) {
+                        ++ ed;
+                    }
+                    ++ ed;
+                }
+
+                for (size_t j = last; j < st; ++ j) {
+                    answer += origin[j];
+                }
+                if (segments[i].second) {
+                    answer += "[";
+                }
+                for (size_t j = st; j < ed; ++ j) {
+                    answer += origin[j];
+                }
+                if (segments[i].second) {
+                    answer += "]";
+                }
+
+                last = ed;
+            }
+            while (last < origin.size()) {
+                answer += origin[last];
+                ++ last;
+            }
+        }
+        if (index < betweens.size()) {
+            answer += betweens[index];
+            ++ index;
+        }
+    }
+    return answer;
+}
+
 int main(int argc, char* argv[])
 {
     int topN;
@@ -63,18 +124,18 @@ int main(int argc, char* argv[])
         cerr << "[usage] <model-file> <rank-list> <top-n> <corpus_in> <segmented_out> <clean_mode>" << endl;
         return -1;
     }
-    
-    
+
+
     string model_path = (string)argv[1];
     SegPhraseParser* parser = new SegPhraseParser(model_path, 0);
     cerr << "parser built." << endl;
-    
+
     loadRankList(argv[2], topN);
     parser->setDict(dict);
-    
+
     FILE* in = tryOpen(argv[4], "r");
     FILE* out = tryOpen(argv[5], "w");
-    
+
     bool clean_mode = (strcmp(argv[6], "0") != 0);
     for (;getLine(in);) {
         vector<string> sentences;
@@ -116,64 +177,12 @@ int main(int argc, char* argv[])
                 }
             }
             vector<pair<string, bool>> segments = parser->segment(text);
-            string answer = "";
-            if (clean_mode) {
-                for (size_t i = 0; i < segments.size(); ++ i) {
-                    if (segments[i].second) {
-                        answer += "[";
-                    }
-                    answer += segments[i].first;
-                    if (segments[i].second) {
-                        answer += "]";
-                    }
-                    answer += " ";
-                }
-                answer += "$ ";
-                corpus += answer;
-            } else {
-                size_t last = 0;
-                if (segments.size() == 0) {
-                    answer += origin;
-                } else {
-                    for (size_t i = 0; i < segments.size(); ++ i) {
-                        size_t st = last;
-                        while (text[st] != segments[i].first[0]) {
-                            ++ st;
-                        }
-                        size_t ed = st;
-                        for (size_t j = 0; j < segments[i].first.size(); ++ j) {
-                            while (text[ed] != segments[i].first[j]) {
-                                ++ ed;
-                            }
-                            ++ ed;
-                        }
-                        
-                        for (size_t j = last; j < st; ++ j) {
-                            answer += origin[j];
-                        }
-                        if (segments[i].second) {
-                            answer += "[";
-                        }
-                        for (size_t j = st; j < ed; ++ j) {
-                            answer += origin[j];
-                        }
-                        if (segments[i].second) {
-                            answer += "]";
-                        }
-                        
-                        last = ed;
-                    }
-                }
-                if (index < betweens.size()) {
-                    answer += betweens[index];
-                    ++ index;
-                }
-                corpus += answer;
-            }
+            string answer = translate(segments, clean_mode, origin, text, betweens, index);
+            corpus += answer;
         }
         fprintf(out, "%s\n", corpus.c_str());
     }
-    
+
     cerr << "[done]" << endl;
     return 0;
 }
